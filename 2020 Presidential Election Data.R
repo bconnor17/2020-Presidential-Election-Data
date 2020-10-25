cfb7d21a17ca163ac156de3a9f6d32cdf587ae3e
Brian J. Connor <brianjconnor17@gmail.com>
2020-10-25 12:08:27 -0400

    Update 2020 Presidential Election Data.R

--- 2020 Presidential Election Data.R
+++ 2020 Presidential Election Data.R
@@ -72,6 +72,85 @@ clinton_bidenPAplot <- ggplot(clinton_bidenPA, aes(x = daystillelection)) +
   xlab("Days until election") +
   labs(title = "FiveThirtyEight Polling Average, 2016 vs. 2020",
        subtitle = "Pennsylvania") +
-  theme(legend.position = "right")
+  theme(legend.position = "right") +
+  geom_hline(yintercept = 50, linetype ="dashed", color = "black")
 
 clinton_bidenPAplot
+
+#Looking at FL movement
+clintonFL <- pollavg2016 %>%
+  filter(state == "Florida") %>%
+  select(daystillelection, clinton)
+
+bidenFL <- pollavg2020 %>%
+  filter(state == "Florida") %>%
+  select(daystillelection, biden)
+
+clinton_bidenFL <- merge(clintonFL, bidenFL,
+                         all.x = TRUE)
+
+clinton_bidenFLplot <- ggplot(clinton_bidenFL, aes(x = daystillelection)) +
+  theme_bw() +
+  geom_line(aes(y = clinton), color = "darkred") +
+  geom_line(aes(y = biden), color ="steelblue") +
+  scale_x_reverse(lim = c(250,0)) +
+  ylab("Democratic share, polling average") +
+  xlab("Days until election") +
+  labs(title = "FiveThirtyEight Polling Average, 2016 vs. 2020",
+       subtitle = "Florida") +
+  theme(legend.position = "right")+
+  geom_hline(yintercept = 50, linetype ="dashed", color = "black")
+
+clinton_bidenFLplot
+
+#Looking at AZ movement
+clintonAZ <- pollavg2016 %>%
+  filter(state == "Arizona") %>%
+  select(daystillelection, clinton)
+
+bidenAZ <- pollavg2020 %>%
+  filter(state == "Arizona") %>%
+  select(daystillelection, biden)
+
+clinton_bidenAZ <- merge(clintonAZ, bidenAZ,
+                         all.x = TRUE)
+
+clinton_bidenAZplot <- ggplot(clinton_bidenAZ, aes(x = daystillelection)) +
+  theme_bw() +
+  geom_line(aes(y = clinton), color = "darkred") +
+  geom_line(aes(y = biden), color ="steelblue") +
+  scale_x_reverse(lim = c(250,0)) +
+  ylab("Democratic share, polling average") +
+  xlab("Days until election") +
+  labs(title = "FiveThirtyEight Polling Average, 2016 vs. 2020",
+       subtitle = "Arizona") +
+  theme(legend.position = "right")+
+  geom_hline(yintercept = 50, linetype ="dashed", color = "black")
+
+clinton_bidenAZplot
+
+#Looking at MI movement
+clintonMI <- pollavg2016 %>%
+  filter(state == "Michigan") %>%
+  select(daystillelection, clinton)
+
+bidenMI <- pollavg2020 %>%
+  filter(state == "Michigan") %>%
+  select(daystillelection, biden)
+
+clinton_bidenMI <- merge(clintonMI, bidenMI,
+                         all.x = TRUE)
+
+clinton_bidenMIplot <- ggplot(clinton_bidenMI, aes(x = daystillelection)) +
+  theme_bw() +
+  geom_line(aes(y = clinton), color = "darkred") +
+  geom_line(aes(y = biden), color ="steelblue") +
+  scale_x_reverse(lim = c(250,0)) +
+  ylab("Democratic share, polling average") +
+  xlab("Days until election") +
+  labs(title = "FiveThirtyEight Polling Average, 2016 vs. 2020",
+       subtitle = "Michigan") +
+  theme(legend.position = "right")+
+  geom_hline(yintercept = 50, linetype ="dashed", color = "black")
+
+clinton_bidenMIplot
