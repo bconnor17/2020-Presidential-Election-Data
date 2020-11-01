0c452f2a31b12437081e7db08e016e724ad3165f
Brian J. Connor <brianjconnor17@gmail.com>
2020-11-01 10:23:19 -0500

    Updated with completed CD and county tool code.

--- Presidential CD and County Result Lookup.R
+++ Presidential CD and County Result Lookup.R
@@ -22,37 +22,41 @@ pres_results_county_clean <- pres_results_county %>%
             republican = sum(republican, na.rm = T),
             green = sum(green, na.rm = T),
             other = sum(other, na.rm = T)) %>%
-  mutate(total = democrat + republican +green + other,
+  mutate(state_po = as.factor(state_po),
+        total = democrat + republican +green + other,
          dem_share = democrat/total,
          rep_share = republican/total,
          abs_diff = abs(dem_share-rep_share))
 
 #Congressional District Lookup Function
 pres_cd_lookup <- function(x,y,z){
-  cd_result <- pres_results_cd[ which(pres_results$state_abb == x &
-                                     pres_results$district == y &
-                                     pres_results$year == z),]
+  cd_result <- pres_results_cd[ which(pres_results_cd$state_abb == x &
+                                     pres_results_cd$district == y &
+                                     pres_results_cd$year == z),]
   rep <- round(cd_result$rep, 4) * 100
   dem <- round(cd_result$dem, 4) * 100
   abs_diff <- round(abs(dem - rep),4)
   
 paste0("In ",cd_result$year,", the Republican presidential candidate won ",
        rep,"% of the vote, and the Democratic candidate won ", 
-       dem,"% of the vote, a lead of ",abs_diff,"%.", collapse = " ")
+       dem,"% of the vote, a difference of ",abs_diff,"%.", collapse = " ")
 }
 
 #County Lookup Function
-pres_cd_lookup <- function(x,y,z){
-  cd_result <- pres_results_cd[ which(pres_results$state_abb == x &
-                                        pres_results$district == y &
-                                        pres_results$year == z),]
-  rep <- round(cd_result$rep, 4) * 100
-  dem <- round(cd_result$dem, 4) * 100
+pres_county_lookup <- function(x,y,z){
+  county_result <- pres_results_county_clean[ which(pres_results_county_clean$state_po == x &
+                                        pres_results_county_clean$county == y &
+                                        pres_results_county_clean$year == z),]
+  rep <- round(county_result$rep_share, 4) * 100
+  dem <- round(county_result$dem_share, 4) * 100
   abs_diff <- round(abs(dem - rep),4)
   
-  paste0("In ",cd_result$year,", the Republican presidential candidate won ",
+  paste0("In ",county_result$year,", the Republican presidential candidate won ",
          rep,"% of the vote, and the Democratic candidate won ", 
-         dem,"% of the vote, a lead of ",abs_diff,"%.", collapse = " ")
+         dem,"% of the vote, a difference of ",abs_diff,"%.", collapse = " ")
 }
 
 pres_cd_lookup("IA", "4", "2016")
+
+pres_county_lookup("WI","Waukesha","2016")
+
